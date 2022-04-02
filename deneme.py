e4b53e785769465ea7f0c93eb272b87ddd389fd5
UmAvallach404 <68709476+UmAvallach404@users.noreply.github.com>
2022-04-02 23:11:50 +0300

    asd
    asd

--- deneme.py
+++ deneme.py
@@ -1,28 +1,59 @@
-import RPi.GPIO as gpio
+GPIO.output(enb,GPIO.HIGH) 
+
+def sag():
+    print("Motorlar sola gidiyor")
+    GPIO.output(in1,GPIO.HIGH)
+    GPIO.output(ena,GPIO.HIGH)
+
+import RPi.GPIO as GPIO
 import time
-def init():
- gpio.setmode(gpio.BCM)
- gpio.setup(17, gpio.OUT)
- gpio.setup(22, gpio.OUT)
- gpio.setup(23, gpio.OUT)
- gpio.setup(24, gpio.OUT)
-def forward(sec):
- init()
- gpio.output(17, True)
- gpio.output(22, False)
- gpio.output(23, True)
- gpio.output(24, False)
- time.sleep(sec)
- gpio.cleanup()
-def reverse(sec):
- init()
- gpio.output(17, False)
- gpio.output(22, True)
- gpio.output(23, False)
- gpio.output(24, True)
- time.sleep(sec)
- gpio.cleanup()
-print("forward")
-forward(4)
-print ("reverse")
-reverse(2)
+
+GPIO.setmode(GPIO.BOARD)
+
+ena = 11
+in2 = 31
+in1 = 29
+in3 = 7
+in4 = 37
+enb = 24
+
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+GPIO.setup(ena, GPIO.OUT)
+GPIO.setup(enb, GPIO.OUT)
+
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
+def dur():
+    print("Motorlar durdu.")
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.LOW)
+    GPIO.output(ena,GPIO.LOW)
+    GPIO.output(enb,GPIO.LOW)
+
+try:
+    while True:
+        ileri()
+        time.sleep(5)
+        dur()
+        time.sleep(7)
+
+        sol()
+        time.sleep(5)
+        dur()
+        time.sleep(7)
+
+        sag()
+        time.sleep(5)
+        dur()
+        time.sleep(7)
+
+
+except:
+    GPIO.cleanup()
