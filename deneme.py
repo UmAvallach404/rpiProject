f4eafceb9e6f01b29d9197c8586562131308f79c
UmAvallach404 <68709476+UmAvallach404@users.noreply.github.com>
2022-04-02 20:13:27 +0300

    2
    dasd

--- deneme.py
+++ deneme.py
@@ -1,12 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
-enable = 11
+GPIO.setmode(GPIO.BOARD)
+
+ena = 11
 input1 = 15
 input2 = 13
 input3 = 16
 input4 = 18
-disable =22
+enb =22
 
 # Input Setup for Motor1
 GPIO.setup(input1, GPIO.OUT)
@@ -17,77 +19,78 @@ GPIO.setup(input3, GPIO.OUT)
 GPIO.setup(input4, GPIO.OUT)
 
 # Motor Activation
-GPIO.setup(enable, GPIO.OUT)
-GPIO.setup(disable, GPIO.OUT)
+GPIO.setup(ena, GPIO.OUT)
+GPIO.setup(enb, GPIO.OUT)
 
+command = input()
 
 def forward():
     print("Motors running for forward movement.")
     GPIO.output(input1, GPIO.HIGH)
     GPIO.output(input2, GPIO.LOW)
-    GPIO.setup(enable, GPIO.HIGH)
+    GPIO.setup(ena, GPIO.HIGH)
 
     GPIO.output(input3, GPIO.HIGH)
     GPIO.output(input4, GPIO.LOW)
-    GPIO.setup(disable, GPIO.HIGH)
+    GPIO.setup(enb, GPIO.HIGH)
 
 def backward():
     print("Motors running for backward movement.")
     GPIO.output(input2, GPIO.HIGH)
     GPIO.output(input1, GPIO.LOW)
-    GPIO.output(enable, GPIO.HIGH)
+    GPIO.output(ena, GPIO.HIGH)
 
     GPIO.output(input4, GPIO.HIGH)
     GPIO.output(input3, GPIO.LOW)
-    GPIO.output(disable, GPIO.HIGH)
+    GPIO.output(enb, GPIO.HIGH)
 
 def right():
     print("Motors running for right movement.")
     GPIO.output(input1, GPIO.HIGH)
     GPIO.output(input2, GPIO.LOW)
-    GPIO.output(enable, GPIO.HIGH)
+    GPIO.output(ena, GPIO.HIGH)
 
     GPIO.output(input4, GPIO.HIGH)
     GPIO.output(input3, GPIO.LOW)
-    GPIO.output(disable, GPIO.HIGH)
+    GPIO.output(enb, GPIO.HIGH)
 
 def left():
     print("Motors running for left movement.")
     GPIO.output(input2, GPIO.HIGH)
     GPIO.output(input1, GPIO.LOW)
-    GPIO.output(enable, GPIO.HIGH)
+    GPIO.output(ena, GPIO.HIGH)
 
     GPIO.output(input3, GPIO.HIGH)
     GPIO.output(input4, GPIO.LOW)
-    GPIO.output(disable, GPIO.HIGH)
+    GPIO.output(enb, GPIO.HIGH)
 
 def stop():
     print("Motors stopped.")
-    GPIO.output(enable, GPIO.LOW)
-    GPIO.output(disable, GPIO.LOW)
+    GPIO.output(ena, GPIO.LOW)
+    GPIO.output(enb, GPIO.LOW)
 
 
-try:
-    while True:
-        forward()
-        time.sleep(3)
-        stop()
-        time.sleep(2)
 
-        backward()
-        time.sleep(3)
-        stop()
-        time.sleep(2)
 
-        right()
-        time.sleep(3)
-        stop()
-        time.sleep(2)
+if command == 'r':
+    forward()
+    time.sleep(3)
+    stop()
+    time.sleep(5)
+    backward()
+    time.sleep(3)
+    stop()
+    time.sleep(5)
+    right()
+    time.sleep(3)
+    stop()
+    time.sleep(5)
+    left()
+    time.sleep(3)
+    stop()
+    time.sleep(5)
 
-        left()
-        time.sleep(3)
-        stop()
-        time.sleep(2)
-
-except:
+if command == 's':
     GPIO.cleanup()
+
+
