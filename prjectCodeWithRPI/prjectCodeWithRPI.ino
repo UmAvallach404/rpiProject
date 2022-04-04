bf7b35530e7e7095a2cf43472f43d929230397ea
UmAvallach404 <68709476+UmAvallach404@users.noreply.github.com>
2022-04-04 20:41:30 +0300

    rpiii
    sad

---
+++ prjectCodeWithRPI/prjectCodeWithRPI.ino
@@ -0,0 +1,129 @@
+
+int analogPin = A0; //arduino pin baglanti yeri
+String inputCommand = "";
+boolean isStringComplete = false;
+int inputValue = 0; // to store the readen value
+
+
+int motor1Pin1 = 2; // pin in1 on L298N
+int motor1Pin2 = 3; // pin in2 on L298N
+int enable1Pin = 9; // pin ena on L298N
+int motor2Pin1 = 4; // pin in3 on L298N
+int motor2Pin2 = 5; // pin in4 on L298N
+int enable2Pin = 10; // pin enb on L298N
+int state;
+int flag=0;        //makes sure that the serial only prints once the state
+int stateStop=0;
+void setup() {
+    // sets the pins as outputs:
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(enable1Pin, OUTPUT);
+    pinMode(motor2Pin1, OUTPUT);
+    pinMode(motor2Pin2, OUTPUT);
+    pinMode(enable2Pin, OUTPUT);
+    // sets enable1Pin and enable2Pin high so that motor can turn on:
+    digitalWrite(enable1Pin, HIGH);
+    digitalWrite(enable2Pin, HIGH);
+    // initialize serial communication at 9600 bits per second:
+    Serial.begin(9600);
+}
+
+  while (Serial.available()<=0){
+    sendStatus();
+    delay(500);
+  }
+
+
+
+
+void loop() {
+    //if some date is sent, reads it and saves in state
+    if(Serial.available() > 0){     
+      state = Serial.read();   
+      flag=0;
+      serialEvent();
+    }   
+    // if the state is 'F' the DC motor will go forward
+    if (inputCommand.startsWith("forward")) {
+        digitalWrite(motor1Pin1, HIGH);
+        digitalWrite(motor1Pin2, LOW); 
+        digitalWrite(motor2Pin1, LOW);
+        digitalWrite(motor2Pin2, HIGH);
+        if(flag == 0){
+          Serial.println("Go Forward!");
+          flag=1;
+        }
+    }
+    
+    // if the state is 'R' the motor will turn left
+    else if (inputCommand.startsWith("left")) {
+        digitalWrite(motor1Pin1, HIGH); 
+        digitalWrite(motor1Pin2, LOW); 
+        digitalWrite(motor2Pin1, LOW);
+        digitalWrite(motor2Pin2, LOW);
+        if(flag == 0){
+          Serial.println("Turn LEFT");
+          flag=1;
+        }
+        delay(1500);
+        state=3;
+        stateStop=1;
+    }
+    // if the state is 'S' the motor will Stop
+    else if (inputCommand.startsWith("stop") || stateStop == 1) {
+        digitalWrite(motor1Pin1, LOW); 
+        digitalWrite(motor1Pin2, LOW); 
+        digitalWrite(motor2Pin1, LOW);
+        digitalWrite(motor2Pin2, LOW);
+        if(flag == 0){
+          Serial.println("STOP!");
+          flag=1;
+        }
+        stateStop=0;
+    }
+    // if the state is 'L' the motor will turn right
+    else if (inputCommand.startsWith("right")) {
+        digitalWrite(motor1Pin1, LOW); 
+        digitalWrite(motor1Pin2, LOW); 
+        digitalWrite(motor2Pin1, LOW);
+        digitalWrite(motor2Pin2, HIGH);
+        if(flag == 0){
+          Serial.println("Turn RIGHT");
+          flag=1;
+        }
+        delay(1500);
+        state=3;
+        stateStop=1;
+    }
+    // if the state is 'B' the motor will Reverse
+    else if (inputCommand.startsWith("backward")) {
+        digitalWrite(motor1Pin1, LOW); 
+        digitalWrite(motor1Pin2, HIGH);
+        digitalWrite(motor2Pin1, HIGH);
+        digitalWrite(motor2Pin2, LOW);
+        if(flag == 0){
+          Serial.println("Reverse!");
+          flag=1;
+        }
+    }
+    //For debugging purpose
+    //Serial.println(state);
+}
+
+void sendStatus(){
+  char buffer[50];
+  inputValue = analogRead(analogPin);
+  sprintf(buffer, "Analog input %d is %d", analogPin, inputValue);
+  Serial.println(buffer);
+}
+
+void serialEvent(){
+  while(Serial.available()){
+    char inputChar = (char)Serial.read();
+    inputCommand += inputChar;
+    if(inputChar == '\n'){
+      isStringComplete = true;
+    }
+  }
+}
