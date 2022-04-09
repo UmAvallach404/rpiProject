6f40d7d5f242b18b4c79f1dd48e9e563abb11fdb
UmAvallach404 <68709476+UmAvallach404@users.noreply.github.com>
2022-04-09 17:01:18 +0300

    lastCode
    i how it works

--- deneme.py
+++ deneme.py
@@ -1,79 +1,21 @@
-import RPi.GPIO as GPIO
-import time 
+import serial
 
-GPIO.setmode(GPIO.BOARD)
-
-ena = 11
-in2 = 31
-in1 = 29
-in3 = 7
-in4 = 37
-enb = 24
-
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(ena,GPIO.OUT)
-GPIO.setup(enb,GPIO.OUT)
-
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-GPIO.output(in3,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-GPIO.output(ena,GPIO.LOW)
-GPIO.output(enb,GPIO.LOW)
-
-def ileri():
-    print("Motorlar ileri gidiyor")
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(ena,GPIO.HIGH)
-
-    GPIO.output(in3,GPIO.HIGH)
-    GPIO.output(enb,GPIO.HIGH) 
-
-
-
-
-def sol():
-    print("Motorlar saga gidiyor")
-
-    GPIO.output(in3,GPIO.HIGH)
-    GPIO.output(enb,GPIO.HIGH) 
-
-def sag():
-    print("Motorlar sola gidiyor")
-    GPIO.output(in1,GPIO.HIGH)
-    GPIO.output(ena,GPIO.HIGH)
-
-
-
-def dur():
-    print("Motorlar durdu.")
-    GPIO.output(in1,GPIO.LOW)
-    GPIO.output(in2,GPIO.LOW)
-    GPIO.output(in3,GPIO.LOW)
-    GPIO.output(in4,GPIO.LOW)
-    GPIO.output(ena,GPIO.LOW)
-    GPIO.output(enb,GPIO.LOW)
-
-try:
-    while True:
-        ileri()
-        time.sleep(5)
-        dur()
-        time.sleep(7)
-
-        sol()
-        time.sleep(5)
-        dur()
-        time.sleep(7)
-
-        sag()
-        time.sleep(5)
-        dur()
-        time.sleep(7)
-
-
-except:
-    GPIO.cleanup()
+x= input()
+if __name__ == '_main_':
+ ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ ser.flush()
+ while True:
+  line = ser.readline().decode('utf-8').rstrip()
+  print(line)
+  if (x == "forward"):
+   ser.write(b"forward/n")
+  elif(x=="right"):
+   ser.write(b"right/n")
+  elif (x == "stop"):
+   ser.write(b"stop/n")
+  elif (x == "left"):
+   ser.write(b"left/n")
+  elif (x == "stop"):
+   ser.write(b"stop/n")
+  else:
+   ser.write(b"stop/n")
